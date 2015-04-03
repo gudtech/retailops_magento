813fe42d8b297907f40048e3dd76e8d24ed9d683
Bogdan Tsyganovskiy <btsyganovskiy@magecore.com>
2015-04-03 12:17:59 +0300

    RET-4: Shipment Push   - Fix retailops order status date output   - License updates

--- app/code/community/RetailOps/Api/Model/Order/Api.php
+++ app/code/community/RetailOps/Api/Model/Order/Api.php
@@ -179,6 +179,7 @@ class RetailOps_Api_Model_Order_Api extends Mage_Sales_Model_Order_Api
             $ordersData = $ordersData['records'];
         }
         $fullResult = array();
+        $fullResult['records'] = array();
         foreach ($ordersData as $orderData) {
             if (isset($orderData['order_increment_id'])) {
                 try {
@@ -206,7 +207,7 @@ class RetailOps_Api_Model_Order_Api extends Mage_Sales_Model_Order_Api
                     $result['status'] = RetailOps_Api_Helper_Data::API_STATUS_FAIL;
                     $result['message'] = $e->getMessage();
                 }
-                $fullResult[] = $result;
+                $fullResult['records'][] = $result;
             }
         }
 
--- app/code/community/RetailOps/Api/Model/Shipment/Api.php
+++ app/code/community/RetailOps/Api/Model/Shipment/Api.php
@@ -33,9 +33,10 @@ class RetailOps_Api_Model_Shipment_Api extends Mage_Sales_Model_Order_Shipment_A
      */
     public function shipmentPush($shipments)
     {
-        $result = array();
-        $count = 0;
+        $fullResult = array();
+        $fullResult['records'] = array();
         foreach ($shipments as $shipmentData) {
+            $result = array();
             try{
                 $shipment = new Varien_Object($shipmentData);
 
@@ -44,7 +45,7 @@ class RetailOps_Api_Model_Shipment_Api extends Mage_Sales_Model_Order_Shipment_A
                     array('record' => $shipment)
                 );
 
-                $result[$count]['order_increment_id'] = $shipment->getOrderIncrementId();
+                $result['order_increment_id'] = $shipment->getOrderIncrementId();
 
                 $orderIncrementId = $shipment->getOrderIncrementId();
                 $shipmentInfo = $shipment->getShipment();
@@ -73,10 +74,10 @@ class RetailOps_Api_Model_Shipment_Api extends Mage_Sales_Model_Order_Shipment_A
                     $shipmentResult['message'] = $e->getMessage();
                 }
 
-                $result[$count]['shipment_result'] = $shipmentResult;
+                $result['shipment_result'] = $shipmentResult;
                 if ($shipmentIncrementId) {
                     if ($trackInfo) {
-                        $result[$count]['track_result'] = array();
+                        $result['track_result'] = array();
                         foreach ($trackInfo as $track) {
                             // add shipment track
                             try {
@@ -101,7 +102,7 @@ class RetailOps_Api_Model_Shipment_Api extends Mage_Sales_Model_Order_Shipment_A
                                 $trackResult['status'] = RetailOps_Api_Helper_Data::API_STATUS_FAIL;
                                 $trackResult['message'] = $e->getMessage();
                             }
-                            $result[$count]['track_result'][] = $trackResult;
+                            $result['track_result'][] = $trackResult;
                         }
                     }
 
@@ -148,17 +149,16 @@ class RetailOps_Api_Model_Shipment_Api extends Mage_Sales_Model_Order_Shipment_A
                             $invoiceResult = $this->_captureInvoices($invoices);
                         }
                     }
-                    $result[$count]['invoice_result'] = $invoiceResult;
+                    $result['invoice_result'] = $invoiceResult;
                 }
             } catch (Exception $e) {
-                $result[$count]['status'] = RetailOps_Api_Helper_Data::API_STATUS_FAIL;
-                $result[$count]['message'] = $e->getMessage();
+                $result['status'] = RetailOps_Api_Helper_Data::API_STATUS_FAIL;
+                $result['message'] = $e->getMessage();
             }
-
-            $count++;
+            $fullResult['records'][] = $result;
         }
 
-        return $result;
+        return $fullResult;
     }
 
     /**
@@ -168,6 +168,7 @@ class RetailOps_Api_Model_Shipment_Api extends Mage_Sales_Model_Order_Shipment_A
     public function orderClose($ordersData)
     {
         $fullResult = array();
+        $fullResult['records'] = array();
         foreach ($ordersData as $orderData) {
             try {
                 if (!empty($orderData['order_increment_id'])) {
@@ -226,7 +227,7 @@ class RetailOps_Api_Model_Shipment_Api extends Mage_Sales_Model_Order_Shipment_A
                 $result['status'] = RetailOps_Api_Helper_Data::API_STATUS_FAIL;
                 $result['message'] = $e->getMessage();
             }
-            $fullResult[] = $result;
+            $fullResult['records'][] = $result;
         }
 
         return $fullResult;
